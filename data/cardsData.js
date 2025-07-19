d7a72a1cf682471e56b7cc946a56aec6b2c44f1d
marika.meidra2003@gmail.com <marika.meidra2003@gmail.com>
2025-07-19 17:18:28 +0400

    rm

--- data/cardsData.js
+++ data/cardsData.js
@@ -1,36 +1,36 @@
 export const cardsData = [
   {
-    src: "../img/Card_Photo_1.png",
+    src: "img/Card_Photo_1.png",
     title: "Taxes & Efficiency",
     category: "Business",
     type: "taxes",
   },
   {
-    src: "../img/Card_Photo_2.png",
+    src: "img/Card_Photo_2.png",
     title: "Investment banking",
     category: "Business",
     type: "banking"
   },
   {
-    src: "../img/Card_Photo_3.png",
+    src: "img/Card_Photo_3.png",
     title: "Audit & Evaluation",
     category: "Business",
     type: "audit"
   },
   {
-    src: "../img/Card_Photo_4.png",
+    src: "img/Card_Photo_4.png",
     title: "Financial Plan",
     category: "Business",
     type: "plan"
   },
   {
-    src: "../img/Card_Photo_5.png",
+    src: "img/Card_Photo_5.png",
     title: "Workspace Design",
     category: "Business",
     type: "design"
   },
   {
-    src: "../img/Card_Photo_6.png",
+    src: "img/Card_Photo_6.png",
     title: "Teamwork",
     category: "Business",
     type: "team"
