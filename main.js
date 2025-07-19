d54dc388eaf777deb9c5b1e6faac85b0e14b0b6a
marika.meidra2003@gmail.com <marika.meidra2003@gmail.com>
2025-07-19 17:15:23 +0400

    fix

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { formFieldsData } from './data/formFieldsData.js'
 import { renderCards, initTabs } from './components/cards.js'
 import { renderContacts } from './components/contacts.js'
 import { renderFormFields, setupFormValidation } from './components/form.js'
-import { initBurgerMenu } from './components/UI/BurgerMenu.js'
+import { initBurgerMenu } from './components/UI/burgerMenu.js'
 
 document.addEventListener('DOMContentLoaded', () => {
   renderCards(cardsData)
